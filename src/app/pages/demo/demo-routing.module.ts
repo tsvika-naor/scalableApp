6d991ec430ae8c0fd9ee89d7bd513feb9bd24bf7
tzvika-naor <zvinaor@gmail.com>
2021-02-10 00:48:57 +0200

    adding styles

--- src/app/pages/demo/demo-routing.module.ts
+++ src/app/pages/demo/demo-routing.module.ts
@@ -5,17 +5,17 @@ import { DemoComponent } from './demo.component';
 const routes: Routes = [
     {
         path: '',
-        component: DemoComponent
-        // children: [
-            // {
-            //     path: 'styles',
-            //     loadChildren: () => import('./pages/styles/styles.module').then(m => m.StylesModule)
-            // },
-            // {
-            //     path: 'shared',
-            //     loadChildren: () => import('./pages/shared/shared.module').then(m => m.SharedModule)
-            // }
-        // ]
+        component: DemoComponent,
+        children: [
+            {
+                path: 'styles',
+                loadChildren: () => import('./pages/styles/styles.module').then(m => m.StylesModule)
+            },
+            {
+                path: 'shared',
+                loadChildren: () => import('./pages/shared/shared.module').then(m => m.SharedModule)
+            }
+        ]
     }
 ];
 
